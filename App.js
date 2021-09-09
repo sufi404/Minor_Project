44b95e4c152b83832ee6fd0b246b988d1c467d71
sufi404 <90145373+sufi404@users.noreply.github.com>
2021-09-09 16:46:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/index'
+import Login from './components/Login/Index'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
